6656c6fb702d68fe10ee874a9b95e9f40cede1b4
rhinamaarin <47165669+rhinamaarin@users.noreply.github.com>
2019-03-05 10:49:48 -0600

    Primer Commit
    El objeto fue creado

---
+++ HospitalitoAyudantia/src/java/Paciente/java.java
@@ -0,0 +1,96 @@
+
+package Paciente;
+
+public class java 
+{
+   private String m_strNombre;
+   private String m_strApellidos;
+   private String m_strGender;
+   private String m_strEdad;
+   private String m_strSintomas;
+   
+   private int m_iBirthYear;
+   private int m_iBirthMonth;
+   private int m_iBirthDay;
+
+    public java(String m_strNombre, String m_strApellidos, String m_strGender, String m_strEdad, String m_strSintomas, int m_iBirthYear, int m_iBirthMonth, int m_iBirthDay) {
+        this.m_strNombre = m_strNombre;
+        this.m_strApellidos = m_strApellidos;
+        this.m_strGender = m_strGender;
+        this.m_strEdad = m_strEdad;
+        this.m_strSintomas = m_strSintomas;
+        this.m_iBirthYear = m_iBirthYear;
+        this.m_iBirthMonth = m_iBirthMonth;
+        this.m_iBirthDay = m_iBirthDay;
+    }
+
+    public String getM_strNombre() {
+        return m_strNombre;
+    }
+
+    public void setM_strNombre(String m_strNombre) {
+        this.m_strNombre = m_strNombre;
+    }
+
+    public String getM_strApellidos() {
+        return m_strApellidos;
+    }
+
+    public void setM_strApellidos(String m_strApellidos) {
+        this.m_strApellidos = m_strApellidos;
+    }
+
+    public String getM_strGender() {
+        return m_strGender;
+    }
+
+    public void setM_strGender(String m_strGender) {
+        this.m_strGender = m_strGender;
+    }
+
+    public String getM_strEdad() {
+        return m_strEdad;
+    }
+
+    public void setM_strEdad(String m_strEdad) {
+        this.m_strEdad = m_strEdad;
+    }
+
+    public String getM_strSintomas() {
+        return m_strSintomas;
+    }
+
+    public void setM_strSintomas(String m_strSintomas) {
+        this.m_strSintomas = m_strSintomas;
+    }
+
+    public int getM_iBirthYear() {
+        return m_iBirthYear;
+    }
+
+    public void setM_iBirthYear(int m_iBirthYear) {
+        this.m_iBirthYear = m_iBirthYear;
+    }
+
+    public int getM_iBirthMonth() {
+        return m_iBirthMonth;
+    }
+
+    public void setM_iBirthMonth(int m_iBirthMonth) {
+        this.m_iBirthMonth = m_iBirthMonth;
+    }
+
+    public int getM_iBirthDay() {
+        return m_iBirthDay;
+    }
+
+    public void setM_iBirthDay(int m_iBirthDay) {
+        this.m_iBirthDay = m_iBirthDay;
+    }
+   
+   
+          
+   
+    
+    
+}
